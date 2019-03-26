20be44cdb2a63c1c9bdf3e8237ef1b0d706f4453
Jamel Reid <jreid@qualityworkscg.com>
2019-03-26 15:02:12 -0500

    iniial commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os"
+
+	"github.com/MrJnrman/gogo/service"
+)
+
+func main() {
+
+	port := os.Getenv("PORT")
+
+	if len(port) == 0 {
+
+		port = "3000"
+
+	}
+
+	server := service.NewServer()
+
+	server.Run(":" + port)
+
+}
